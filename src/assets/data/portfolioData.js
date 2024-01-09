e1532c723d4ffa80e0d1ade20f769e4ec339fb7d
astkout <astkout12@hotmail.com>
2024-01-09 14:36:42 +0000

    cmt12

--- src/assets/data/portfolioData.js
+++ src/assets/data/portfolioData.js
@@ -3,6 +3,7 @@ import tindog from "../images/tindog.png";
 import simon from "../images/simon.png";
 import keeper from "../images/keeper.png";
 import portfolio from "../images/portfolio.png";
+import medicare from "../images/medicare.png"
 
 const portfolios = [
   {
@@ -10,9 +11,8 @@ const portfolios = [
     imgUrl: tindog,
     category: "Web Design",
     title: "Tindog",
-    description:
-      "A tinder site but....for dogs !!!",
-    technologies: ["Html", "Css",],
+    description: "A tinder site but....for dogs !!!",
+    technologies: ["Html", "Css"],
     siteUrl: "https://tindog-pffl-astkout.vercel.app/",
     siteUrla: "https://github.com/astkout/tindog",
   },
@@ -21,10 +21,9 @@ const portfolios = [
     imgUrl: medicare,
     category: "Web Design",
     title: "Medicare",
-    description:
-      "My first medical page.",
-    technologies: ["React", "Tailwind", "Node.js",],
-    siteUrl: "https://medicare-git-main-astkout.vercel.app/home",
+    description: "My first medical page.",
+    technologies: ["React", "Tailwind", "Node.js"],
+    siteUrl: "https://medicare-dun.vercel.app/",
     siteUrla: "https://github.com/astkout/medicare",
   },
   {
@@ -32,23 +31,10 @@ const portfolios = [
     imgUrl: keeper,
     category: "Ux",
     title: "Keeper App.",
-    description:
-      "A daily note app that will keep your thoughts on track.",
-    technologies: ["Javascript", "CSS","Html"],
+    description: "A daily note app that will keep your thoughts on track.",
+    technologies: ["Javascript", "CSS", "Html"],
     siteUrl: "https://keeper-git-main-astkout.vercel.app/",
-    siteUrla:"https://github.com/astkout/Keeper",
-  }
-  ,
-  {
-    id: "03",
-    imgUrl: simon,
-    category: "Ux",
-    title: "Simon Says Game.",
-    description:
-      "A Simon says game to spend your time with.",
-    technologies: ["Javascript", "Html", "CSS"],
-    siteUrl: "https://simonsays-hazel.vercel.app/",
-    siteUrla: "https://github.com/astkout/simonsays",
+    siteUrla: "https://github.com/astkout/Keeper",
   },
   {
     id: "04",
@@ -60,20 +46,39 @@ const portfolios = [
     technologies: ["Html", "CSS", "Javascript"],
     siteUrl: "https://astkout.github.io/DrumKit/",
     siteUrla: "https://github.com/astkout/DrumKit",
-  }
- ,
-//  {
-//   id: "05",
-//   imgUrl: drumkit,
-//   category: "Ux",
-//   title: "Drumkit Interactive Game.",
-//   description:
-//     "An interactive Drum Kit web app which you can have fun with :)",
-//   technologies: ["Html", "CSS", "Javascript"],
-//   siteUrl: "https://astkout.github.io/DrumKit/",
-//   siteUrla: "https://github.com/astkout/DrumKit",
-// }
-
+  },
+  {
+    id: "05",
+    imgUrl: simon,
+    category: "Ux",
+    title: "Simon Says Game.",
+    description: "A Simon says game to spend your time with.",
+    technologies: ["Javascript", "Html", "CSS"],
+    siteUrl: "https://simonsays-hazel.vercel.app/",
+    siteUrla: "https://github.com/astkout/simonsays",
+  },
+  {
+    id: "06",
+    imgUrl: portfolio,
+    category: "Ux",
+    title: "Portfolio",
+    description:
+      "My first portfolio",
+    technologies: ["React", "SCSS", "API"],
+    siteUrl: "https://portfolio-ruddy-zeta-63.vercel.app/",
+    siteUrla: "https://github.com/astkout/portfolio",
+  },
+  // {
+  //   id: "05",
+  //   imgUrl: drumkit,
+  //   category: "Ux",
+  //   title: "Drumkit Interactive Game.",
+  //   description:
+  //     "An interactive Drum Kit web app which you can have fun with :)",
+  //   technologies: ["Html", "CSS", "Javascript"],
+  //   siteUrl: "https://astkout.github.io/DrumKit/",
+  //   siteUrla: "https://github.com/astkout/DrumKit",
+  // },
 
   //   {
   //     id: "06",
